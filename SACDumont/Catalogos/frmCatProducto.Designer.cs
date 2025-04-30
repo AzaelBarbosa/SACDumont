51cd7a6f89fcf34f7e037ecc621701d06b9961d4
azaelbarbosa <azael.barbosa89@gmail.com>
2025-04-30 00:33:07 -0500

    Cambios

---
+++ SACDumont/Catalogos/frmCatProducto.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace SACDumont.Catalogos
+{
+    partial class frmCatProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbProductoID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbProductoID
+            // 
+            this.lbProductoID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbProductoID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbProductoID.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lbProductoID.Location = new System.Drawing.Point(517, 524);
+            this.lbProductoID.Name = "lbProductoID";
+            this.lbProductoID.Size = new System.Drawing.Size(100, 23);
+            this.lbProductoID.TabIndex = 34;
+            this.lbProductoID.Text = "lbProductoID";
+            this.lbProductoID.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // frmCatProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 556);
+            this.Controls.Add(this.lbProductoID);
+            this.Name = "frmCatProducto";
+            this.Text = "frmCatProducto";
+            this.Load += new System.EventHandler(this.frmCatProducto_Load);
+            this.Controls.SetChildIndex(this.lbProductoID, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbProductoID;
+    }
+}
---
+++ SACDumont/Catalogos/frmCatProducto.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SACDumont.Base;
+using SACDumont.Clases;
+using SACDumont.Modulos;
+
+namespace SACDumont.Catalogos
+{
+    public partial class frmCatProducto : frmBaseCatalogos
+    {
+        #region "Variables"
+        DataTable dtProductos = new DataTable("Productos");
+        int idProducto = 0;
+        basFunctions basFunctions = new basFunctions();
+        sqlServer sqlServer = new sqlServer();
+        #endregion
+        #region "Métodos Virtuales"
+
+        protected override void Nuevo()
+        {
+            frmCatProducto frm = new frmCatProducto(0);
+            frm.ShowDialog();
+        }
+        protected override void Guardar()
+        {
+            // Implementar la lógica para guardar el registro de producto
+        }
+        protected override void Eliminar()
+        {
+            // Implementar la lógica para eliminar el registro de producto
+        }
+        protected override void Acciones()
+        {
+            // Implementar la lógica para imprimir el listado de productos
+        }
+        protected override void Cerrar()
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        #region "Métodos"
+
+        private void CargarProductos()
+        {
+            // Implementar la lógica para cargar los productos en el DataTable
+        }
+
+        private void AsignarValoresProducto(DataRow row)
+        {
+            // Implementar la lógica para asignar valores a los controles del formulario
+        }
+        private void LimpiarControles()
+        {
+            // Implementar la lógica para limpiar los controles del formulario
+        }
+        private void CargarProducto(int idProducto)
+        {
+            if (idProducto == 0)
+            {
+                // Cargar un nuevo registro vacío
+                this.Text = "Nuevo Producto";
+                LimpiarControles();
+                lbProductoID.Text = "0";
+            }
+            else
+            {
+                // Cargar el registro de producto existente
+                string sSQL = "SELECT * FROM Productos WHERE id_producto = " + idProducto;
+                dtProductos = sqlServer.ExecSQLReturnDT(sSQL, "Productos");
+                this.Text = "Editar Producto";
+                if (dtProductos.Rows.Count > 0)
+                {
+                    DataRow row = dtProductos.Rows[0];
+                    AsignarValoresProducto(row);
+                }
+            }
+        }
+        private void CargarMenu()
+        {
+            btAddTutor.Visible = false;
+        }
+        #endregion
+
+        public frmCatProducto(int idProducto)
+        {
+            InitializeComponent();
+            this.idProducto = idProducto;
+        }
+
+        private void frmCatProducto_Load(object sender, EventArgs e)
+        {
+            CargarMenu();
+        }
+    }
+}
